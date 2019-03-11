751b7b0888ab747579a83119b5b5b78b61840c56
van Hauser <vh@thc.org>
2019-03-11 06:21:21 -0400

    fix

--- afl-fuzz-dyninst.sh
+++ afl-fuzz-dyninst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test -z "$1" -o "$1" = "-h" && { echo Syntax: $0 afl-fuzz-options ; echo sets the afl-dyninst environment variables ; exit 1 ; }
-sysctl -w kernel.core_pattern="core" > /dev/null
+sysctl -w kernel.core_pattern=core > /dev/null
 sysctl -w kernel.randomize_va_space=0 > /dev/null
 echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor > /dev/null
 export AFL_SKIP_BIN_CHECK=1
