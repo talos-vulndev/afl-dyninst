50025b8bbe518c26f520e90f9e278f299a164ee4
van Hauser <vh@thc.org>
2020-02-07 12:05:15 +0100

    warning fix

--- afl-fuzz-dyninst.sh
+++ afl-fuzz-dyninst.sh
@@ -6,7 +6,7 @@ test -z "$1" -o "$1" = "-h" && { echo Syntax: $0 afl-fuzz-options ; echo sets th
 export AFL_SKIP_BIN_CHECK=1
 export DYNINSTAPI_RT_LIB=/usr/local/lib/libdyninstAPI_RT.so
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:.
-echo " $*" | grep -q " -m" && { echo "Warning: no -m memory option specified!" ; sleep 1 ; }
+echo " $*" | grep -q " -m" || { echo "Warning: no -m memory option specified!" ; sleep 1 ; }
 #export AFL_EXIT_WHEN_DONE=1
 #export AFL_TMPDIR=/run/$$
 #export AFL_PRELOAD=./desock.so:./libdislocator/libdislocator.so
