cff2d88f97f983683f4caf0e8f87a4f858b486be
vanhauser-thc <vh@thc.org>
2021-05-21 22:29:38 +0200

    support new afl++ working

--- afl-fuzz-dyninst.sh
+++ afl-fuzz-dyninst.sh
@@ -4,6 +4,7 @@ test -z "$1" -o "$1" = "-h" && { echo Syntax: $0 afl-fuzz-options ; echo sets th
 # remember to run afl-system-config !
 #
 export AFL_SKIP_BIN_CHECK=1
+export AFL_MAP_SIZE=65536
 export DYNINSTAPI_RT_LIB=/usr/local/lib/libdyninstAPI_RT.so
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:.
 echo " $*" | grep -q " -m" || { echo "Warning: no -m memory option specified!" ; sleep 1 ; }
